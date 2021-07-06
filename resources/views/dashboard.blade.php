0f789b1dd74b061eec6582109ffa42b7afccfd9a
poorvi gorakhpuriya <purvig27@gmail.com>
2021-07-06 11:28:15 +0530

    Dashboard updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
                         <p class="top-all">Partner Portal</p>
                     </div>
-                    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                    <div class="col-lg-6 col-md-6 col-sm-12x col-xs-12">
                         <div class="header-top-wraper">
                             <div class="row">
                                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
